b25e3e9c5ebcb65f7d404c10306d085501e553ec
EvelinSu <eveelinsu@gmail.com>
2023-02-11 23:58:17 +0300

    added navigate to /maps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import { StyledWelcomeButton } from '@/modules/welcome/styles/StyledWelcomeButton';
 import { StyledWelcomePage } from '@/modules/welcome/styles/StyledWelcomePage';
 import { Inter } from '@next/font/google';
+import {useRouter} from "next/router";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    const navigate = useRouter()
+
   return (
       <StyledWelcomePage>
-        <StyledWelcomeButton>
+        <StyledWelcomeButton onClick={() => navigate.push('/maps')}>
           Go!
         </StyledWelcomeButton>
       </StyledWelcomePage>
