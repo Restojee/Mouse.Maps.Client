c8ce7d3fbf05047f147a5183ae876144c27faa17
EvelinSu <108629994+EvelinSu@users.noreply.github.com>
2023-02-20 21:40:45 +0300

    Issue/ms 8 (#3)
    [MS-8] added app icons

--- src/layout/panel/Panel.tsx
+++ src/layout/panel/Panel.tsx
@@ -1,48 +1,89 @@
-import { Avatar } from "@/layout/avatar/Avatar";
-import { NavLink } from "@/layout/navigation/NavLink";
-import { MoonIcon } from "@/svg/MoonIcon";
-import { StyledNavLinkSection } from "@/layout/navigation/styles/StyledNavLinkSection";
-import { StyledPanel } from "@/layout/panel/styles/StyledPanel";
+import {Avatar} from "@/layout/avatar/Avatar";
+import {NavLink} from "@/layout/navigation/NavLink";
+import {MoonIcon} from "@/svg/MoonIcon";
+import {StyledNavLinkSection} from "@/layout/navigation/styles/StyledNavLinkSection";
+import {StyledPanel} from "@/layout/panel/styles/StyledPanel";
+import {BurgerIcon} from "@/svg/BurgerIcon";
+import {NotificationsIcon} from "@/svg/NotificationIcons";
+import {ChartIcon} from "@/svg/ChartIcon";
+import {PaperIcon} from "@/svg/PaperIcon";
+import {ChatFillIcon} from "@/svg/ChatFillIcon";
+import {SettingsIcon} from "@/svg/SettingsIcon";
+import {LogInIcon} from "@/svg/LogInIcon";
 
 type PanelProps = {
     activeTab: string;
     isOpen: boolean;
 }
 export const Panel = (props: PanelProps) => {
+
     return (
         <StyledPanel>
             <NavLink
-                label="Все карты"
+                label="Открыть/Скрыть панель"
                 prepend={
                     <StyledNavLinkSection>
-                        <MoonIcon />
+                        <BurgerIcon />
                     </StyledNavLinkSection>
                 }
             />
             <Avatar size="46px" />
             <NavLink
-                label="Все карты"
+                label="Уведомления"
                 prepend={(
                     <StyledNavLinkSection>
-                        <MoonIcon />
+                        <NotificationsIcon />
                     </StyledNavLinkSection>
                 )}
             />
             <NavLink
-                label="Сменить тему"
-                prepend={ (
+                label="Полезная инфа"
+                prepend={(
                     <StyledNavLinkSection>
-                        <MoonIcon />
+                        <PaperIcon />
                     </StyledNavLinkSection>
-                ) }
+                )}
             />
             <NavLink
-                label="Сменить тему"
-                prepend={ (
+                label="Статистика"
+                prepend={(
                     <StyledNavLinkSection>
-                        <MoonIcon />
+                        <ChartIcon />
                     </StyledNavLinkSection>
-                ) }
+                )}
+            />
+            <NavLink
+                label="Чат"
+                prepend={(
+                    <StyledNavLinkSection>
+                        <ChatFillIcon />
+                    </StyledNavLinkSection>
+                )}
+            />
+            <NavLink
+                label="Настройки"
+                margin={"auto 0 0 0"}
+                prepend={(
+                    <StyledNavLinkSection>
+                        <SettingsIcon />
+                    </StyledNavLinkSection>
+                )}
+            />
+            <NavLink border
+                     label="Сменить тему"
+                     prepend={(
+                         <StyledNavLinkSection>
+                             <MoonIcon />
+                         </StyledNavLinkSection>
+                     )}
+            />
+            <NavLink
+                label="Выйти/войти"
+                prepend={(
+                    <StyledNavLinkSection>
+                        <LogInIcon />
+                    </StyledNavLinkSection>
+                )}
             />
         </StyledPanel>
     )
